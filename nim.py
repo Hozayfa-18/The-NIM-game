4169211a5228910329e47336292eb9f318631b9c
Hozayfa-18 <80411230+Hozayfa-18@users.noreply.github.com>
2024-05-17 10:57:07 +0200

    Add files via upload

---
+++ nim.py
@@ -0,0 +1,204 @@
+import math
+import random
+import time
+
+
+class Nim():
+
+    def __init__(self, initial=[1, 3, 5, 7]):
+        """
+        Initialize game board.
+        """
+        self.piles = initial.copy()
+        self.player = 0
+        self.winner = None
+
+    @classmethod
+    def available_actions(cls, piles):
+        actions = set()
+        for i, pile in enumerate(piles):
+            for j in range(1, pile + 1):
+                actions.add((i, j))
+        return actions
+
+    @classmethod
+    def other_player(cls, player):
+        return 0 if player == 1 else 1
+
+    def switch_player(self):
+        self.player = Nim.other_player(self.player)
+
+    def move(self, action):
+        pile, count = action
+
+        # Check for errors
+        if self.winner is not None:
+            raise Exception("Game already won")
+        elif pile < 0 or pile >= len(self.piles):
+            raise Exception("Invalid pile")
+        elif count < 1 or count > self.piles[pile]:
+            raise Exception("Invalid number of objects")
+
+        # Update pile
+        self.piles[pile] -= count
+        self.switch_player()
+
+        # Check for a winner
+        if all(pile == 0 for pile in self.piles):
+            self.winner = self.player
+
+
+class NimAI():
+
+    def __init__(self, alpha=0.5, epsilon=0.1):
+        self.q = dict()
+        self.alpha = alpha
+        self.epsilon = epsilon
+
+    def update(self, old_state, action, new_state, reward):
+        old = self.get_q_value(old_state, action)
+        best_future = self.best_future_reward(new_state)
+        self.update_q_value(old_state, action, old, reward, best_future)
+
+    def get_q_value(self, state, action):
+        if (tuple(state), action) in self.q:
+            return self.q[tuple(state), action]
+        else:
+            return 0
+
+
+    def update_q_value(self, state, action, old_q, reward, future_rewards):
+
+        self.q[(tuple(state), action)] = old_q + self.alpha * (future_rewards + reward - old_q)
+
+
+    def best_future_reward(self, state):
+        reward = 0
+        actions = list(Nim.available_actions(state))
+        for action in actions:
+            reward = max(self.get_q_value(state, action), reward)
+        return reward
+
+    def choose_action(self, state, epsilon=True):
+        top_action = None
+        reward = 0
+        actions = list(Nim.available_actions(state))
+        for action in actions:
+            q_value = self.get_q_value(state, action)
+            if top_action is None or q_value > reward:
+                reward = q_value
+                top_action = action
+        if epsilon:
+            action_length = len(actions)
+            weights = [(1 - self.epsilon) if action == top_action else self.epsilon for action in actions]
+            top_action = random.choices(actions, weights=weights, k=1)[0]
+
+        return top_action
+
+
+def train(n):
+
+    player = NimAI()
+
+    # Play n games
+    for i in range(n):
+        print(f"Playing training game {i + 1}")
+        game = Nim()
+
+        # Keep track of last move made by either player
+        last = {
+            0: {"state": None, "action": None},
+            1: {"state": None, "action": None}
+        }
+
+        # Game loop
+        while True:
+
+            # Keep track of current state and action
+            state = game.piles.copy()
+            action = player.choose_action(game.piles)
+
+            # Keep track of last state and action
+            last[game.player]["state"] = state
+            last[game.player]["action"] = action
+
+            # Make move
+            game.move(action)
+            new_state = game.piles.copy()
+
+            # When game is over, update Q values with rewards
+            if game.winner is not None:
+                player.update(state, action, new_state, -1)
+                player.update(
+                    last[game.player]["state"],
+                    last[game.player]["action"],
+                    new_state,
+                    1
+                )
+                break
+
+            # If game is continuing, no rewards yet
+            elif last[game.player]["state"] is not None:
+                player.update(
+                    last[game.player]["state"],
+                    last[game.player]["action"],
+                    new_state,
+                    0
+                )
+
+    print("Done training")
+
+    # Return the trained AI
+    return player
+
+
+def play(ai, human_player=None):
+
+    # If no player order set, choose human's order randomly
+    if human_player is None:
+        human_player = random.randint(0, 1)
+
+    # Create new game
+    game = Nim()
+
+    # Game loop
+    while True:
+
+        # Print contents of piles
+        print()
+        print("Piles:")
+        for i, pile in enumerate(game.piles):
+            print(f"Pile {i}: {pile}")
+        print()
+
+        # Compute available actions
+        available_actions = Nim.available_actions(game.piles)
+        time.sleep(1)
+
+        # Let human make a move
+        if game.player == human_player:
+            print("Your Turn")
+            while True:
+                pile = int(input("Choose Pile: "))
+                count = int(input("Choose Count: "))
+                if (pile, count) in available_actions:
+                    break
+                print("Invalid move, try again.")
+
+        # Have AI make a move
+        else:
+            print("AI's Turn")
+            pile, count = ai.choose_action(game.piles, epsilon=False)
+            print(f"AI chose to take {count} from pile {pile}.")
+
+        # Make move
+        game.move((pile, count))
+
+        # Check for winner
+        if game.winner is not None:
+            print()
+            print("GAME OVER")
+            winner = "Human" if game.winner == human_player else "AI"
+            print(f"Winner is {winner}")
+            return
+    
